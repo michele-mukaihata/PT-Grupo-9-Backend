4e399ad30dc59915244cd0b7fa82d38c34a777bc
Mariana Farias <marianafarias.oficial1@gmail.com>
2025-11-03 18:22:30 -0300

    Pequena alteração no service

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProdutoService {
+  constructor(private prisma: PrismaService) {}
+
   create(createProdutoDto: CreateProdutoDto) {
     return 'This action adds a new produto';
   }
 
   findAll() {
     return `This action returns all produto`;
+    //return this.prisma.produto;
   }
 
   findOne(id: number) {
