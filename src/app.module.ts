e8b3f5f0a3fc0225409cbf6fa381b159cb2a5ed9
Michele Aiko <micheleaikomukaihata@gmail.com>
2025-11-12 11:40:53 -0300

    :sparkles: feat: Criação da estrutura básica do CRUD de avaliações

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ImagensProdutoModule } from './imagens_produto/imagens_produto.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { LojasModule } from './lojas/lojas.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { ComentariosModule } from './comentarios/comentarios.module';
 
 @Module({
-  imports: [ProdutoModule, ImagensProdutoModule, UsuariosModule, LojasModule, CategoriasModule],
+  imports: [ProdutoModule, ImagensProdutoModule, UsuariosModule, LojasModule, CategoriasModule, ComentariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
